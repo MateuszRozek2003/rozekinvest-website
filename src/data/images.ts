000bced3eff27eddbf2a3c9926e978ce893f3095
MateuszRozek2003 <mateusz@rozek.pl>
2026-06-04 19:08:37 +0200

    Update images.ts

--- src/data/images.ts
+++ src/data/images.ts
@@ -28,7 +28,7 @@ export const nautilusApartmentImages = [
 // (Wyświetlane na stronie głównej w karcie "Vale Furado" i po kliknięciu "Powiększ")
 // ----------------------------------------------------------------------
 export const valefuradoApartmentImages = [
-"/1V.jpg",
+"/20V.jpg",
   "/2V.jpg",
   "/3V.jpg",
   "/4V.jpg",
