ca1eb51feadd162be1d858ece0c1d76c3481e699
MateuszRozek2003 <mateusz@rozek.pl>
2026-06-07 11:40:24 +0200

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -11,12 +11,7 @@ export function Hero() {
     <section className="relative min-h-[90vh] flex items-center pt-32 sm:pt-40 pb-20 overflow-hidden">
       {/* Background Image / Video */}
       <div className="absolute inset-0 z-0 overflow-hidden bg-black">
-        <iframe
-          src="https://www.youtube.com/embed/phwymplGT5Q?autoplay=1&mute=1&loop=1&playlist=phwymplGT5Q&controls=0&rel=0&iv_load_policy=3&modestbranding=1&playsinline=1&vq=hd2160&start=10&end=155&disablekb=1"
-          allow="autoplay; encrypted-media"
-          className="absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 pointer-events-none scale-[1.3]"
-          style={{ width: "100vw", height: "56.25vw", minHeight: "100vh", minWidth: "177.77vh" }}
-        />
+<img src="/tlo.jpg" className="absolute top-1/2 left-1/2 min-w-full min-h-full -translate-x-1/2 -translate-y-1/2 object-cover" alt="Główne zdjęcie basenu" />
         <div className="absolute inset-0 bg-black/70" />
       </div>
 
